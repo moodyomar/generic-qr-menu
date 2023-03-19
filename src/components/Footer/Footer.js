9aeb27c515318f71b8e353cb8b5b6d38df74b12c
Mohammad Omar Shihab <lonak.secret@gmail.com>
2023-03-19 14:33:44 +0000

    add svg logo to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
-
-
 const Footer = () => { 
 
 return(
 
 <div className='Footer' style={footerStyle}>
 <p>פיתוח על ידי <a href="https://qbmedia.co.il" target="_blank" rel="noreferrer" style={footerStyle.a}>קיובי מדיה</a></p>
+<a href="https://qbmedia.co.il" target="_blank" rel="noreferrer">
+    <img src={process.env.PUBLIC_URL + 'qbmedia.svg'} width={125} alt="QB Media" style={footerStyle.img}/>
+</a>
 </div>
 
 )
@@ -17,12 +18,16 @@ const footerStyle = {
     height:"100px",
     color:"white",
     display:"flex",
+    flexDirection:'column',
     justifyContent:"center",
     alignItems:"center",
     a:{
         textDecoration:"none",
         color:"#D39F47",
         pointer:"cursor"
+    },
+    img:{
+        marginTop:'10px'
     }
 }
 
